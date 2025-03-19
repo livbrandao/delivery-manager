6a7fd602547fbf146b314c5c1f265449dfbe9f75
livbrandao <liviatoledo27@gmail.com>
2025-03-19 00:03:24 -0300

    :package: build: adiciona deploy e gh-pages

--- src/app/orders/[id]/page.tsx
+++ src/app/orders/[id]/page.tsx
@@ -1,9 +1,8 @@
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 // import { OrderDetailsPage } from '@/pages/OrderDetailsPage';
 
 export default function OrderDetails() {
-  const router = useRouter();
-  const { id } = router.query;
-
-  return; // <OrderDetailsPage orderId={id} />
+  // const router = useRouter();
+  // const { id } = router.query;
+  // return; // <OrderDetailsPage orderId={id} />
 }

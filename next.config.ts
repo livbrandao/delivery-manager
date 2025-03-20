1698349dbd3ef1dca39203c0fdfeba3450e31bc1
livbrandao <liviatoledo27@gmail.com>
2025-03-20 17:49:23 -0300

    :bricks: ci: Modificação no next.config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/delivery-manager',
-  assetPrefix: '/delivery-manager/',
+  basePath: '',
+  assetPrefix: '',
   images: {
     unoptimized: true,
   },

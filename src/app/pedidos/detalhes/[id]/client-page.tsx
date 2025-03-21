70de32780d36b7790605e8e8869486dfffe43f46
livbrandao <liviatoledo27@gmail.com>
2025-03-21 00:29:12 -0300

    :lipstick: feat: Estilização CSS se caso retorno de error na página de detalhes

--- src/app/pedidos/detalhes/[id]/client-page.tsx
+++ src/app/pedidos/detalhes/[id]/client-page.tsx
@@ -8,6 +8,7 @@ import OrderDetails from '@/components/orders/OrderDetails';
 import { Order } from '@/types/Order';
 import { getOrderById } from '@/utils/api';
 import { HiArrowLeft } from 'react-icons/hi';
+import { MdError } from 'react-icons/md';
 
 interface ClientOrderDetailsPageProps {
   id: string;
@@ -56,12 +57,15 @@ export default function ClientOrderDetailsPage({ id }: ClientOrderDetailsPagePro
 
   if (error) {
     return (
-      <div className="bg-white p-6 rounded-lg shadow-md">
-        <h1 className="text-xl font-medium text-red-600 mb-4">Erro</h1>
+      <div className="bg-white p-6 rounded-lg shadow-md py-16 mt-24 flex flex-col items-center space-y-6">
+        <div className="flex items-center space-x-4 bg-red-100 p-4 rounded-full">
+          <MdError size={48} className="text-red-600" />
+          <h1 className="text-xl font-bold text-red-600  uppercase">Erro</h1>
+        </div>
         <p className="text-purple-700 mb-6">{error}</p>
         <Button
           onClick={() => router.push('/pedidos')}
-          variant="primary"
+          variant="outline"
           className="flex items-center space-x-2 "
         >
           <HiArrowLeft size={20} className="mr-2" /> Voltar para a lista de pedidos
